e1a4960b44215279f15625780e8536168066b8b5
BuildTools <unconfigured@null.spigotmc.org>
2018-07-01 09:56:02 -0500

    working

---
+++ Driver.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using System;
+
+// Full credit to this awesome blog post I used http://algassert.com/post/1718
+
+namespace Quantum.ShorQsharp
+{
+    class Driver
+    {
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            int factorme = 15;  //note: trying to factor 21 takes abt 10 minutes per attempt on my PC.
+
+            using (var sim = new QuantumSimulator())
+            {
+                while (true)
+                {
+                    var testradix = ChooseRandomCoprime(factorme);
+
+                    Console.WriteLine("Starting Quantum Routine: radix = {0}, modulus = {1}", testradix, factorme);
+                    long res = SamplePeriod.Run(sim, testradix, factorme).Result;
+                    Console.WriteLine("Result {0}", res);
+                    if ((res != 1) && (res != factorme - 1) && (res * res % factorme == 1))
+                    {
+                        Console.WriteLine("Factors Found: {0} x {1} = {2}", Gcd(res + 1, factorme), Gcd(res - 1, factorme), factorme);
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    }
+                } 
+            }
+        }
+
+        static long Gcd(long x, long y)
+        {
+            return y == 0 ? x : Gcd(y, x % y);
+        }
+
+        static int ChooseRandomCoprime(int x)
+        {
+            int y = rnd.Next(x/3, x - 2);
+            return Gcd(x, y) == 1 ? y : ChooseRandomCoprime(x);
+        }
+    }
+}
---
+++ Operation.qs
@@ -0,0 +1,43 @@
+﻿namespace Quantum.ShorQsharp
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Extensions.Math;
+
+	operation SamplePeriod (radix: Int, modulus: Int ) : (Int)
+    {
+        body
+        {
+			AssertBoolEqual(IsCoprime(radix,modulus), true, "Radix and modulus must be coprime.");
+
+			mutable measuredPeriod = 0;
+			let registersize = 2*BitSize(modulus)+1;
+
+			using ( targetState = Qubit[BitSize(modulus)] )
+			{
+				using ( controlRegister = Qubit[registersize] )
+				{
+					IntegerIncrementLE( 1 , LittleEndian(targetState) );
+					QuantumPhaseEstimation( DiscreteOracle(PowerOracle(radix,modulus,_,_)) , LittleEndian(targetState) , BigEndian(controlRegister) );
+					set measuredPeriod = MeasureIntegerBE( BigEndian(controlRegister) );
+					ResetAll(targetState);
+				}
+			}
+
+		    let cleanedPeriod = AbsI(Snd( ContinuedFractionConvergent( Fraction(measuredPeriod, 2^(registersize)) , modulus ) )) ;			
+			
+			return  ExpMod ( radix, cleanedPeriod/2 , modulus );
+		}
+    }
+
+	operation PowerOracle (radix: Int, modulus: Int, power: Int, target: Qubit[]) : ()
+	{
+		body
+		{
+			ModularMultiplyByConstantLE( ExpMod(radix,power,modulus), modulus, LittleEndian(target));
+		}
+		adjoint auto
+		controlled auto
+		adjoint controlled auto
+	}
+}
